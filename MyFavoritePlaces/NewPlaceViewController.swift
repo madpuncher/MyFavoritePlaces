0836eabbc244c29c89d2ff0130a46055c7dab3f7
Eldar Tengizov <75984018+madpuncher@users.noreply.github.com>
2021-07-24 19:05:25 +0300

    Added ImagePickerController

--- MyFavoritePlaces/NewPlaceViewController.swift
+++ MyFavoritePlaces/NewPlaceViewController.swift
@@ -17,6 +17,22 @@ class NewPlaceViewController: UITableViewController {
 
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         if indexPath.row == 0 {
+            let alert = UIAlertController(title: nil, message: nil, preferredStyle: .actionSheet)
+            
+            let library = UIAlertAction(title: "Photo", style: .default) { [weak self] _ in
+                self?.chooseImagePicker(source: .photoLibrary)
+            }
+            let camera = UIAlertAction(title: "Camera", style: .default) { [weak self] _ in
+                self?.chooseImagePicker(source: .camera)
+            }
+            
+            let cancel = UIAlertAction(title: "Cancel", style: .cancel)
+            
+            alert.addAction(library)
+            alert.addAction(camera)
+            alert.addAction(cancel)
+            
+            present(alert, animated: true)
             
         } else {
             view.endEditing(true)
@@ -30,5 +46,18 @@ extension NewPlaceViewController: UITextFieldDelegate {
     func textFieldShouldReturn(_ textField: UITextField) -> Bool {
         textField.resignFirstResponder()
         return true
+    }
+}
+
+
+extension NewPlaceViewController {
+    
+    func chooseImagePicker(source: UIImagePickerController.SourceType) {
+        if UIImagePickerController.isSourceTypeAvailable(source) {
+            let picker = UIImagePickerController()
+            picker.allowsEditing = true
+            picker.sourceType = source
+            present(picker, animated: true)
+        }
     }
 }
