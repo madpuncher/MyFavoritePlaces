30d49de451e878c12ed96f8430882397de5547f5
Eldar Tengizov <75984018+madpuncher@users.noreply.github.com>
2021-07-24 18:16:54 +0300

    Design for NewPlaceVC

---
+++ MyFavoritePlaces/NewPlaceViewController.swift
@@ -0,0 +1,34 @@
+//
+//  NewPlaceViewController.swift
+//  MyFavoritePlaces
+//
+//  Created by Eʟᴅᴀʀ Tᴇɴɢɪᴢᴏᴠ on 24.07.2021.
+//
+
+import UIKit
+
+class NewPlaceViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        tableView.tableFooterView = UIView()
+    }
+
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        if indexPath.row == 0 {
+            
+        } else {
+            view.endEditing(true)
+        }
+    }
+
+}
+
+extension NewPlaceViewController: UITextFieldDelegate {
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
+}
