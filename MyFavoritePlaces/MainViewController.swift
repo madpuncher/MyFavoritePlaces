4207f266e30533273711201ac164f381e3d6b3ec
Eldar Tengizov <75984018+madpuncher@users.noreply.github.com>
2021-07-24 14:13:08 +0300

    Design the app

--- MyFavoritePlaces/MainViewController.swift
+++ MyFavoritePlaces/MainViewController.swift
@@ -18,14 +18,13 @@ class MainViewController: UITableViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        
+        //
     }
     
     // MARK: - Table view data source
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        favoriteRestaurants.count
+        return favoriteRestaurants.count
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
@@ -33,9 +32,16 @@ class MainViewController: UITableViewController {
         
         cell.textLabel?.text = favoriteRestaurants[indexPath.row]
         cell.imageView?.image = UIImage(named: favoriteRestaurants[indexPath.row])
+        cell.imageView?.layer.cornerRadius = cell.frame.size.height / 2
+        cell.imageView?.clipsToBounds = true
         return cell
     }
     
+    //MARK: - Table view delegate
+    
+    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 85
+    }
     
     /*
      // MARK: - Navigation
