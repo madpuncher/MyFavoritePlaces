a1792ad6c34cc2ca6bc175257436e42bda9bad52
Eldar Tengizov <75984018+madpuncher@users.noreply.github.com>
2021-07-27 17:29:01 +0300

    User location setup

--- MyFavoritePlaces/MapViewController.swift
+++ MyFavoritePlaces/MapViewController.swift
@@ -7,11 +7,13 @@
 
 import UIKit
 import MapKit
+import CoreLocation
 
 class MapViewController: UIViewController {
     
     var currentPlace = Place()
     let annotationIdentifier = "annotationIdentifier"
+    let locationManager = CLLocationManager()
     
     @IBOutlet weak var mapView: MKMapView!
     
@@ -19,19 +21,22 @@ class MapViewController: UIViewController {
         super.viewDidLoad()
         setupLocation()
         mapView.delegate = self
+        checkLocationService()
     }
     
     private func setupLocation() {
         guard let location = currentPlace.location else { return }
         
         let geocoder = CLGeocoder()
-        geocoder.geocodeAddressString(location) { [weak self] placemarks, error in
+        geocoder.geocodeAddressString(location) { [weak self] (placemarks, error) in
+            
             if let error = error {
                 print(error.localizedDescription)
                 return
             }
             
             guard let placemarks = placemarks else { return }
+            
             let placemark = placemarks.first
             
             let annotation = MKPointAnnotation()
@@ -39,6 +44,7 @@ class MapViewController: UIViewController {
             annotation.subtitle = self?.currentPlace.type
             
             guard let placemarkLocation = placemark?.location else { return }
+            
             annotation.coordinate = placemarkLocation.coordinate
             
             self?.mapView.showAnnotations([annotation], animated: true)
@@ -47,16 +53,50 @@ class MapViewController: UIViewController {
         
     }
     
+    private func checkLocationService() {
+        
+        if CLLocationManager.locationServicesEnabled() {
+            setupLocationManager()
+            checkLocationAuth()
+        } else {
+            
+        }
+    }
+    
+    private func setupLocationManager() {
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        locationManager.delegate = self
+    }
+    
     @IBAction func cancellAction() {
         dismiss(animated: true)
     }
     
+    private func checkLocationAuth() {
+        switch CLLocationManager.authorizationStatus() {
+        case .authorizedWhenInUse:
+            mapView.showsUserLocation = true
+            break
+        case .denied:
+            //
+            break
+        case .notDetermined:
+            locationManager.requestWhenInUseAuthorization()
+        case .restricted:
+            break
+        case .authorizedAlways:
+            break
+        @unknown default:
+            print("Have new case")
+        }
+    }
 }
 
 //MARK: - Extension for MapKit delegate
 extension MapViewController: MKMapViewDelegate {
     
     func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
+        
         guard !(annotation is MKUserLocation) else { return nil }
         
         var annotationView = mapView.dequeueReusableAnnotationView(withIdentifier: annotationIdentifier) as? MKPinAnnotationView
@@ -67,6 +107,7 @@ extension MapViewController: MKMapViewDelegate {
         }
         
         if let data = currentPlace.image {
+            
             let imageView = UIImageView(frame: CGRect(x: 0, y: 0, width: 50, height: 50))
             imageView.layer.cornerRadius = 10
             imageView.clipsToBounds = true
@@ -74,5 +115,12 @@ extension MapViewController: MKMapViewDelegate {
             annotationView?.rightCalloutAccessoryView = imageView
         }
         return annotationView
+    }
+}
+
+extension MapViewController: CLLocationManagerDelegate {
+    
+    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+        checkLocationAuth()
     }
 }
